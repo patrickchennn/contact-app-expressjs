4140dad008eb830c83423e3fc861e3242492e105
patrickchennn <patrick.chen31@gmail.com>
2022-06-25 15:34:32 +0700

    set a main html template

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import * as contact from "./utils/contacts.js";
 
+import express from "express";
+const app = express();
+const port = 3000;
 
 app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
-
-  res.render("index",{test:"oi"});
+  res.render("layouts/main", {
+    title:"index page",
+    page:"index.ejs"
+  });
 })
 
 app.get('/about', (req, res) => {
-  res.render("about");
-})
+  res.render("layouts/main", {
+    title:"about page",
+    page:"about.ejs"
+  });
+});
 
 app.get('/contacts', (req, res) => {
-  res.render("contacts");
-})
+  res.render("layouts/main", {
+    title:"contacts page",
+    page:"contacts.ejs"
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
---
+++ utils/contacts.js
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+import { constants } from 'buffer';
+import chalk from 'chalk';
+
+const orangeColor = chalk.hex("#FFA500");
+const contactPath = "./data/contacts.json";
+
+fs.access(contactPath, constants.F_OK, err => {
+  // if file(contactPath) does not exist
+  if(err){
+    // then create a file with contactPath as a path
+    // and put the user input data in it
+    // CREATE
+    console.log(chalk.red(`[-] contacts.json does not exist`));
+    fs.mkdir("./data", err => {
+      console.log(chalk.green(`[+] created automatically in ${contactPath}`));;
+    });
+    fs.writeFileSync(contactPath, `[]`, err => {
+      if(err) throw err;
+    });
+  }
+});
+
+export const getContact = () => {
+  const rawData = fs.readFileSync(contactPath,{encoding:"utf-8"});
+  const currContact = JSON.parse(rawData);
+
+  return currContact;
+}
