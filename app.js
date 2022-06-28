61a56793524eed5d09523db0088deda1204d09f6
patrickchennn <patrick.chen31@gmail.com>
2022-06-28 11:33:19 +0700

    add a add contact feature

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import * as con from "./utils/contacts.js";
 import express from "express";
-
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
 app.set("view engine","ejs");
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/', (req, res) => {
   res.render("layouts/main", {
@@ -19,16 +24,36 @@ app.get('/about', (req, res) => {
     page:"about.ejs"
   });
 });
+
+
+// send contact data
+app.post('/add-contact', (req,res) => {
+  console.log(req.body);
+  con.addContact(req.body);
+  res.redirect("/contacts");
+  // res.send(req.body);
+});
+
+
+// add contact
+app.get('/add-contact', (req, res) => {
+  res.render("layouts/main", {
+    title:"Add Contact",
+    page:"add-contact.ejs",
+    
+  });
+});
+
 app.get('/contacts', (req, res) => {
   const contacts = con.getContacts();
-  
   res.render("layouts/main", {
-    title:"contacts page",
+    title:"Contacts List",
     page:"contacts.ejs",
     contacts
   });
 });
 
+// get contact detail
 app.get('/contacts/:name', (req, res) => {
   const namae = req.params.name;
   console.log(namae);
--- utils/contacts.js
+++ utils/contacts.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import { constants } from 'buffer';
 import chalk from 'chalk';
 
-const orangeColor = chalk.hex("#FFA500");
+const orenji = chalk.hex("#FFA500");
 const contactPath = "./data/contacts.json";
 
 fs.access(contactPath, constants.F_OK, err => {
@@ -32,4 +32,48 @@ export const getSpecificContact = (namae) => {
   const contacts = getContacts();
   return (contacts.find(contact => contact.name === namae));
 
+}
+
+/**
+ * @contact an object that to be expected contains a name, email, phone number
+ */
+ export const addContact = (contact) => {
+  fs.access(contactPath, constants.F_OK, err => {
+    // if file(contactPath) does not exist
+    if(err){
+      // then create a file with contactPath as a path
+      // and put the user input data in it
+      console.log(chalk.red(`[-] contacts.json does not exist`));
+      fs.mkdir("./data", err => {
+        console.log(chalk.green(`[+] created automatically in ${contactPath}`));;
+      });
+      fs.writeFileSync(contactPath, `[${JSON.stringify(contact)}]`, err => {
+        if(err) throw err;
+      });
+    }
+    // if file(contactPath) does exist
+    else{
+      // then insert a new contact
+
+      const currContact = getContacts();
+
+      // before insert. check possible duplicate contact
+      currContact.forEach(curr => {
+        if(curr["name"]===contact["name"]){
+          throw console.error(chalk.red(`[-] duplicate name is not possible!`));
+          
+        }
+      });
+
+      // insert part
+      currContact.push(contact);
+      
+      // write to file part
+      fs.writeFileSync(contactPath, JSON.stringify(currContact,null,'\t'), err => {
+        if(err) throw err;
+      });
+
+
+    }
+  })
 }
