49aaaf330cb0f80abeb2cd58c76340c2c5e84878
patrickchennn <patrick.chen31@gmail.com>
2022-06-30 18:25:24 +0700

    add delete and edit contact feature

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ import * as con from "./utils/contacts.js";
 import express from "express";
 import bodyParser from "body-parser";
 import chalk from "chalk";
-const orenji = chalk.hex("#FFA500");
-
 import validator from "validator";
-
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import flash from "connect-flash";
 
+const orenji = chalk.hex("#FFA500");
+const ungu = chalk.magenta;
+
 const app = express();
 const port = 3000;
+app.listen(port, () => {
+  console.log(`app is listening on port localhost:${port}`)
+});
 
+// template engine
 app.set("view engine","ejs");
 
+// middleware
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -30,7 +35,7 @@ app.use(session({
   saveUninitialized:true,
 }));
 
-// root page
+// root/index page
 app.get('/', (req, res) => {
   res.render("layouts/main", {
     title:"index page",
@@ -47,10 +52,7 @@ app.get('/about', (req, res) => {
 });
 
 // send contact data
-app.post(
-  '/add-contact',
-  // callback
-  (req,res) => {
+app.post('/add-contact', (req,res) => {
     const data = req.body;
     console.log(orenji(JSON.stringify(data,null,'\t')));
 
@@ -87,15 +89,14 @@ app.post(
       console.log(chalk.green("[+] not error"));
       console.log(data);
       con.addContact(data);
-      req.flash("msg","data contact is registered!")
+      req.flash("msg","data contact is deleted!");
       res.redirect("/contacts");
     }
 
   }
 );
 
-
-// add contact
+// add-contact page
 app.get('/add-contact', (req, res) => {
   res.render("layouts/main", {
     title:"Add Contact",
@@ -103,6 +104,7 @@ app.get('/add-contact', (req, res) => {
   });
 });
 
+// contacts page
 app.get('/contacts', (req, res) => {
   const contacts = con.getContacts();
   res.render("layouts/main", {
@@ -113,20 +115,102 @@ app.get('/contacts', (req, res) => {
   });
 });
 
-// get contact detail with given id
-app.get('/contacts/:name', (req, res) => {
-  const namae = req.params.name;
-  console.log(namae);
+// delete contact with name as an id
+app.get('/contacts/delete/:namae', (req, res) => {
+  const namae = req.params.namae;
+  console.log(orenji(`[*] deleting contact with name: ${namae}`));
+
+  // const contact = con.getSpecificContact(namae);
+  const contact = con.deleteContact(namae);
+
+  if(!contact){
+    res.status(404);
+    res.send("<h1>404</h1>");
+  }else{
+    req.flash("msg",`${namae} contact is deleted!`);
+    res.redirect("/contacts");
+  }
+});
+
+// TODO: delete all contacts
+
+
+// edit-contact page
+// edit contact with name as an id
+app.get('/contacts/edit-contact/:namae', (req, res) => {
+  const namae = req.params.namae;
+  console.log(orenji(`[*] editing contact with name: ${namae}`));
+
   const contact = con.getSpecificContact(namae);
-  console.log(contact);
+  if(!contact){
+    res.status(404);
+    res.send("<h1>404</h1>");
+  }else{
+    res.render("layouts/main", {
+      title:"Edit Contact",
+      page:"edit-contact.ejs",
+      contact,
+    });
+  }
+});
+
+// send the new data(edit page)
+app.post('/contacts/edit-contact', (req,res) => {
+  const data = req.body;
+  console.log(ungu(`[info] new data:\n${JSON.stringify(data,null,'\t')}`));
+  console.log(data.oldName);
+  const errorMaps = new Map();
+  
+  // email is not required.
+  // If the user decide to put email, check it 
+  if(data.email.length > 0 && !validator.isEmail(data.email)){
+    // and put it on map if it's invalid.
+    errorMaps.set("email",data.email);
+  }
+
+  // phone number is required.
+  // If the phone number is invalid,
+  if(!validator.isMobilePhone(data.phoneNum,'id-ID')){
+    errorMaps.set("phoneNum",data.phoneNum);
+  }
+
+  // if there is a duplicate name
+  if(data.oldName!=data.name && con.isDuplicate("name",data.name)){
+    errorMaps.set("name",data.name);
+  }
+
+  console.log(errorMaps);
+  // if there is an error
+  if (errorMaps.size > 0) {
+    console.log(chalk.red("[-] error at editing"));
+    res.render("layouts/main",{
+      title:"Add Contact",
+      page:"edit-contact.ejs",
+      errorMaps,
+      contact:data,
+    });
+  }else{
+    console.log(chalk.green("[+] no error at editing"));
+    con.updateContacts(data);
+    req.flash("msg","data contact is updated!");
+    res.redirect("/contacts");
+  }
+});
+
+
+// detail page
+// get contact detail page with name as an id
+app.get('/contacts/:namae', (req, res) => {
+  const namae = req.params.namae;
+  const contact = con.getSpecificContact(namae);
+  console.log(`
+    detail contact for:
+    ${chalk.cyan(JSON.stringify(contact,null,'\t'))}
+  `);
   res.render("layouts/main", {
     title:"contacts page",
     page:"detail.ejs",
     contact,
     namae
   });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port localhost:${port}`)
 });
--- utils/contacts.js
+++ utils/contacts.js
@@ -5,86 +5,112 @@ import chalk from 'chalk';
 const orenji = chalk.hex("#FFA500");
 const contactPath = "./data/contacts.json";
 
+// if file(contactPath) does not exist
 fs.access(contactPath, constants.F_OK, err => {
-  // if file(contactPath) does not exist
   if(err){
     // then create a file with contactPath as a path
-    // and put the user input data in it
-    // CREATE
     console.log(chalk.red(`[-] contacts.json does not exist`));
     fs.mkdir("./data", err => {
       console.log(chalk.green(`[+] created automatically in ${contactPath}`));;
     });
-    fs.writeFileSync(contactPath, `[]`, err => {
-      if(err) throw err;
-    });
+    // and put the data in that file afterwise
+    saveContact("[]");
   }
 });
 
-export const getContacts = () => {
-  const rawData = fs.readFileSync(contactPath,{encoding:"utf-8"});
-  const currContact = JSON.parse(rawData);
-
-  return currContact;
-}
-
-export const getSpecificContact = (namae) => {
-  const contacts = getContacts();
-  return (contacts.find(contact => contact.name === namae));
-
+/**
+ * utility function for editing file
+ */
+export const saveContact = (contact) => {
+  fs.writeFileSync(contactPath,contact, err => {
+    if(err) throw err;
+  });
 }
 
 /**
- * @contact an object that to be expected contains a name, email, phone number
+ * fetch data from contacts.json
  */
- export const addContact = (contact) => {
-  fs.access(contactPath, constants.F_OK, err => {
-    // if file(contactPath) does not exist
-    if(err){
-      // then create a file with contactPath as a path
-      // and put the user input data in it
-      console.log(chalk.red(`[-] contacts.json does not exist`));
-      fs.mkdir("./data", err => {
-        console.log(chalk.green(`[+] created automatically in ${contactPath}`));;
-      });
-      fs.writeFileSync(contactPath, `[${JSON.stringify(contact)}]`, err => {
-        if(err) throw err;
-      });
-    }
-    // if file(contactPath) does exist
-    else{
-      // then insert a new contact
+export const getContacts = () => {
+  const rawData = fs.readFileSync(contactPath,{encoding:"utf-8"});
+  const currContact = JSON.parse(rawData);
+  return currContact;
+}
 
-      const currContact = getContacts();
+export const addContact = (contact) => {
+  if(isDuplicate("name",contact.name)) return;
+  
+  const contacts = getContacts();
+  
+  // insert part
+  contacts.push(contact);
+  
+  // write to file part
+  saveContact(JSON.stringify(currContact,null,'\t'));
+}
 
-      // before insert. check possible duplicate contact
-      currContact.forEach(curr => {
-        if(curr["name"]===contact["name"]){
-          throw console.error(chalk.red(`[-] duplicate name is not possible!`));
-          
-        }
-      });
-
-      // insert part
-      currContact.push(contact);
-      
-      // write to file part
-      fs.writeFileSync(contactPath, JSON.stringify(currContact,null,'\t'), err => {
-        if(err) throw err;
-      });
-
-
-    }
-  })
-};
-
+/**
+ * returns undefined if no elements are found.
+ */
+export const getSpecificContact = (namae) => {
+  const contacts = getContacts();
+  return (contacts.find(contact => contact.name === namae));
+}
 
 export const isDuplicate = (attr,value) => {
   const contacts = getContacts();
   for(const contact of contacts){
-    if(contact[attr] === value){
-      return true;
-    }
+    if(contact[attr] === value) return true;
   }
   return false;
 };
+
+export const updateContacts = (editedContact) => {
+  const contacts = getContacts();
+  console.log(chalk.magenta(`[info] updateContact() is called]`));
+
+  const idx = contacts.findIndex(c => c.name===editedContact.oldName);
+  console.log("indx" + idx);
+  // if we could not find the name
+  if(idx === -1){
+    // then log it
+    console.error(
+      // if name is not defined(name===undefined)
+      ( chalk.white.bgRed.bold(`[-] ${editedContact.oldName} is not found `))
+    );
+    // and then return false to indicate that the contact does not exist
+    return;
+  }
+  delete editedContact.oldName;
+  contacts[idx] = editedContact;
+  console.log(contacts);
+  saveContact(JSON.stringify(contacts,null,'\t'));
+}
+
+export const deleteAllContacts = () => saveContact("[]");
+
+export const deleteContact = (name) => {
+  const contacts = getContacts();
+  const idx = contacts.findIndex(c => c.name===name);
+
+  // if we could not find the name
+  if(idx === -1){
+    // then log it
+    console.error(
+      // if name is not defined(name===undefined)
+      ( chalk.white.bgRed.bold(`[-] ${!name ? "please specify name to be deleted" : `${name} is not found`}`) )
+    );
+    // and then return false to indicate that the contact does not exist
+    return false;
+  }
+
+  // if we find the name, then remove the contact
+  contacts.splice(idx,1); // this method is where we actually remove the contact
+  // console.log(
+  //   orenji(JSON.stringify(contacts,null,'\t'))
+  // );
+
+  saveContact(JSON.stringify(contacts,null,'\t'));
+
+  console.log(chalk.green(`[+] ${name} is deleted from contact.`));
+  return true;
+}
